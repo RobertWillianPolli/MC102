3e4b6e4f594ad54d2565fca096d17ee61b1ebe91
Robert Polli <robert.polli@hotmail.com>
2021-03-26 20:27:34 -0300

    Added lab01

---
+++ Laboratorios/Lab01/lab01.py
@@ -0,0 +1,20 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 1 - Aritmética com Inteiros
+# Nome: ROBERT WILLIAN POLLI
+# RA: 187848
+###################################################
+
+a = int(input())
+b = int(input())
+c = int(input())
+
+print("a =", a)
+print("b =", b)
+print("c =", c)
+print("a + b + c =", a + b + c)
+print("a - b - c =", a - b - c)
+print("a * b * c =", a * b * c)
+print("a ** c =", a ** c)
+print("b // c =", b // c)
+print("b % c =", b % c)
