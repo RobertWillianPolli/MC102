a63d075dd7f1c7ca4bef3861a4f0872a73c7165d
Robert Polli <robert.polli@hotmail.com>
2021-04-01 20:18:49 -0300

    Added lab 02

---
+++ Laboratorios/Lab02/lab02.py
@@ -0,0 +1,28 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 2 - O Grande Show
+# Nome: Robert Willian Polli
+# RA: 187848
+###################################################
+
+# Leitura de dados
+dist_a_b = int(input())
+vel_t1 = float(input())
+t_b_c = int(input()) #Tempo que o segundo trem sairá da estação B
+
+dist_b_c = int(input())
+vel_t2 = float(input())
+t_show = int(input())
+
+# Cálculo do tempo de chegada na cidade B e de chegada na cidade C
+
+t_AB = (dist_a_b/vel_t1)*60
+t_BC = (dist_b_c/vel_t2)*60
+t_saidaTrem = t_b_c - t_AB
+
+# Impressão da resposta
+
+if (t_saidaTrem < 0) or ((t_AB + t_BC + t_saidaTrem) > t_show):
+    print(False)
+else:
+    print(True)
