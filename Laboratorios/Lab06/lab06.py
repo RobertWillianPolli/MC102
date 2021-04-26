e81c6ad119eb748fff8eccc4e45db320cb672c5c
Robert Polli <robert.polli@hotmail.com>
2021-04-26 20:28:33 -0300

    Added lab 06

---
+++ Laboratorios/Lab06/lab06.py
@@ -0,0 +1,34 @@
+###################################################
+# MC102 - Algoritmos e Programação de Computadores
+# Laboratório 6 - Plataforma
+# Nome: Robert Willian Polli
+# RA: 187848
+###################################################
+
+# Leitura de dados
+
+plataforma = [int(i) for i in input().split()]
+i = int(input()) - 1
+
+positions = [i]
+state = ''
+
+while(True):
+    new_pos = i + plataforma[i]
+    if new_pos > len(plataforma):
+        state = 'direita'
+        break
+    elif new_pos < 0:
+        state = 'esquerda'
+        break
+    else:
+        if new_pos in positions:
+            state = 'loop'
+            break
+        else:
+            positions.append(new_pos)
+            i = new_pos
+
+for posicoes in positions:
+    print(posicoes+1)
+print(state)
