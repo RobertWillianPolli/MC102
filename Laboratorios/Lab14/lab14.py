5a0b395388ce2e965a830c0d9441214bf3a53aba
Robert Polli <robert.polli@hotmail.com>
2021-06-27 12:39:55 -0300

    Lab 14 ainda

--- Laboratorios/Lab14/lab14.py
+++ Laboratorios/Lab14/lab14.py
@@ -5,9 +5,8 @@
 # RA: 187848
 ###################################################
 
-commands = ["A", "E", "O", "C", "P", "S", "T"]
 padroes = {"A":range(10),
-           "E":range(0, 10, 2),
+           "E":range(2, 10, 2),
            "O":range(1, 10, 2),
            "C":[4, 6, 8, 9],
            "P":[2, 3, 5, 7],
@@ -15,48 +14,45 @@ padroes = {"A":range(10),
            "T":[3, 6, 9]
            }
 
-"""
-Esta função recebe como parâmetro uma matriz, uma posição inicial na
-matriz determinada pelos parâmetros linha e coluna e um padrão que
-deve ser buscado em todas as direções (norte, sul, leste, oeste,
-nordeste, sudeste, noroeste e sudoeste) a partir da posição inicial.
-Caso o padrão seja encontrado a partir da posição inicial a função
-deve retornar o valor True. Caso contrário, a função de retornar o
-valor False.
-"""
-def busca_padrao(linha, coluna, padrao):
-    p = []
-    for i in [-1, 0, 1]:
-        for j in [-1, 0, 1]:
-            if not((linha + i < 0) or (coluna + j < 0) or (linha + i > (m_linhas-1)) or (coluna + j > (m_colunas-1)) or (i == j == 0)):
-                if int(matriz[linha + i][coluna + j]) in padroes[padrao]:
-                    p.append([linha + i, coluna + j])
-    return(p)
-
-def check(p):
-    global ordem
-    ordem += 1
-
-    for positions in p:
-        possibilidades = busca_padrao(positions[0], positions[1], pattern[ordem])
-
-        if ordem == (len(pattern) - 1):
-            if possibilidades != []:
-                return(True)
-            else:
-                return(False)
-        else:
-            return(check(possibilidades))
-
-# Leitura da matriz
-
 matriz = []
 pattern = ""
 received = "Null"
 
+def busca_padrao(linha, coluna, padrao):
+    pp = []
+    for i in [-1, 0, 1]:
+        for j in [-1, 0, 1]:
+            if not((linha + i < 0) or (coluna + j < 0) or (linha + i > (m_linhas-1)) or (coluna + j > (m_colunas-1)) or (i == j == 0)):
+                if int(matriz[linha + i][coluna + j]) in padroes[padrao]:
+                    pp.append([linha + i, coluna + j])
+    return(pp)
+
+def check(p, my_ordem):
+
+    my_ordem += 1
+
+    for positions in p:
+        possibilidades = busca_padrao(positions[0], positions[1], pattern[my_ordem])
+
+        #print(f"Para a posição: {positions}\nPadrao procurado: {pattern[my_ordem]}")
+        #print("Possibilidades:")
+        #for i in possibilidades:
+        #    print(i)
+
+        if my_ordem == (len(pattern)-1):
+            if possibilidades != []:
+                return(True)
+            else:
+                return(False)
+
+        elif check(p = possibilidades, my_ordem = my_ordem):
+            return(True)
+
+# Leitura da matriz
+
 while(True):
     received = input()
-    if not(received[0] in commands):
+    if not(received[0] in padroes):
         matriz.append(received.split(" "))
     else:
         pattern = received
@@ -70,19 +66,14 @@ possibilidades          = []
 posicoes_iniciais       = []
 possibilidades_iniciais = []
 
-global ordem
-
 for linha in range(m_linhas):
     for coluna in range(m_colunas):
-        ordem = 1
         if int(matriz[linha][coluna]) in padroes[pattern[0]]:
             possibilidades_iniciais = busca_padrao(linha, coluna, pattern[1])
-            if possibilidades_iniciais != []:
-                if check(possibilidades_iniciais):
+
+            if check(p = possibilidades_iniciais, my_ordem=1):
+                if not([linha+1, coluna+1] in posicoes_iniciais):
                     posicoes_iniciais.append([linha+1, coluna+1])
-
-
-#print(posicoes_iniciais)
 
 # Impressão das posições iniciais (linha e coluna)
 if posicoes_iniciais == []:
