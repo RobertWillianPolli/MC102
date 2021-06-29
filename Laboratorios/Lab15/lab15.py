52f1be19d9b78e0b89549d3967307d142516aadd
Robert Willian Polli <robert.polli@cnpem.br>
2021-06-29 13:42:45 -0300

    Continuando lab 15

--- Laboratorios/Lab15/lab15.py
+++ Laboratorios/Lab15/lab15.py
@@ -1,8 +1,8 @@
 ###################################################
 # MC102 - Algoritmos e Programação de Computadores
 # Laboratório 15 - De salto em salto
-# Nome:
-# RA:
+# Nome: Robert Willian Polli    Data: 29/06/2021
+# RA: 187848
 ###################################################
 
 
@@ -18,24 +18,22 @@ def propagacao_de_fake_news(matriz, x, y, alcance_value=0):
         alcance = int(matriz[x][y])
 
     def percorrer(inicio_x, fim_x, inicio_y, fim_y, passo_x, passo_y):
-        print(alcance)
         print(inicio_x, fim_x, passo_x, inicio_y, fim_y, passo_y)
         for coordenada_y in range(inicio_y, fim_y, passo_y):
             for coordenada_x in range(inicio_x, fim_x, passo_x):
                 print(coordenada_x, coordenada_y)
-                print(m_colunas, m_linhas)
-                if (coordenada_x > m_colunas) or (coordenada_y > m_linhas) or matriz[coordenada_x][coordenada_y] == "#":
+
+                #print(m_colunas, m_linhas)
+                if not(coordenada_x in range(m_linhas)) or not(coordenada_y in range(m_colunas)) or matriz[coordenada_x][coordenada_y] == "#":
                     break
 
                 elif matriz[coordenada_x][coordenada_y].isdigit():
                     novo_alcance = int(matriz[coordenada_x][coordenada_y])
                     matriz[coordenada_x][coordenada_y] = "X"
-                    propagacao_de_fake_news(matriz=matriz, x=x, y=y + alcance, alcance_value=novo_alcance)
-#                except:
-#                    return()
+                    propagacao_de_fake_news(matriz=matriz, x=coordenada_x, y=coordenada_y, alcance_value=novo_alcance)
 
-    percorrer(x, x + 1, y, y - alcance - 1,  1, -1)  # Norte
-    percorrer(x, x + 1, y, y + alcance + 1,  1,  1)  # Sul
+    #percorrer(x, x + 1, y, y - alcance - 1,  1, -1)  # Norte
+    #percorrer(x, x + 1, y, y + alcance + 1,  1,  1)  # Sul
     percorrer(x, x + alcance + 1, y, y + 1,  1,  1)  # Leste
     percorrer(x, x - alcance - 1, y, y + 1, -1,  1)  # Oeste
 
