7af866d8199a188bb4a656975f9ba26036dcde98
Robert Polli <robert.polli@hotmail.com>
2021-06-29 20:48:08 -0300

    Added lab 15

--- Laboratorios/Lab15/lab15.py
+++ Laboratorios/Lab15/lab15.py
@@ -5,7 +5,6 @@
 # RA: 187848
 ###################################################
 
-
 '''
 Dada uma matriz e a posição (x,y) de uma possível pessoa infectada,
 atualiza a matriz para que cada caractere correspondente a uma pessoa
@@ -14,26 +13,24 @@ infectada seja substituído por 'X'.
 def propagacao_de_fake_news(matriz, x, y, alcance_value=0):
     alcance = alcance_value
 
-    if alcance_value == 0:
+    if alcance_value == 0 and matriz[x][y].isdigit():
         alcance = int(matriz[x][y])
 
     def percorrer(inicio_x, fim_x, inicio_y, fim_y, passo_x, passo_y):
-        print(inicio_x, fim_x, passo_x, inicio_y, fim_y, passo_y)
+
         for coordenada_y in range(inicio_y, fim_y, passo_y):
             for coordenada_x in range(inicio_x, fim_x, passo_x):
-                print(coordenada_x, coordenada_y)
 
-                #print(m_colunas, m_linhas)
                 if not(coordenada_x in range(m_linhas)) or not(coordenada_y in range(m_colunas)) or matriz[coordenada_x][coordenada_y] == "#":
-                    break
+                    return()
 
                 elif matriz[coordenada_x][coordenada_y].isdigit():
                     novo_alcance = int(matriz[coordenada_x][coordenada_y])
                     matriz[coordenada_x][coordenada_y] = "X"
                     propagacao_de_fake_news(matriz=matriz, x=coordenada_x, y=coordenada_y, alcance_value=novo_alcance)
 
-    #percorrer(x, x + 1, y, y - alcance - 1,  1, -1)  # Norte
-    #percorrer(x, x + 1, y, y + alcance + 1,  1,  1)  # Sul
+    percorrer(x, x + 1, y, y - alcance - 1,  1, -1)  # Norte
+    percorrer(x, x + 1, y, y + alcance + 1,  1,  1)  # Sul
     percorrer(x, x + alcance + 1, y, y + 1,  1,  1)  # Leste
     percorrer(x, x - alcance - 1, y, y + 1, -1,  1)  # Oeste
 
